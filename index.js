2aac01896643b05dab55aa585558de23aa55c043
Lara Sanders <larasanders@gmail.com>
2018-06-24 21:45:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
-let neighborhoodId = 0
-let customerId = 0
-let mealId = 0
-let deliveryId = 0
-
-class Neighborhood {
-  constructor(name) {
-    this.name = name
-    this.id = neighborhoodId++
-    store.neighborhoods.push(this)
-  }
-
-  deliveries() {
-    return store.deliveries.filter(delivery => {
-      return.delivery.neighborhoodId === this.id
-    })
-  }
-  customers() {
-    return store.deliveries().map(delivery => {
-      return.delivery.customer()
-    })
-  }
-}
-
-class Customer {
-  constructor(name, neighborhood) {
-    this.name = name
-    this.neighborhoodId = neighborhood.id
-    this.id = customerId++
-    store.customers.push(this)
-  }
-}
-
-class Meal {
-  constructor(title, price) {
-    this.title = title
-    this.price = price
-    this.id = mealId++
-    store.meals.push(this)
-  }
-}
-
-class Delivery {
-  constructor(mealId, neighborhoodId, customerId) {
-    this.mealId = meal.id
-    this.neighborhoodId = neighborhood.id
-    this.customerId = customer.id
-    this.id = deliveryId++
-    store.delivery.push(this)
-  }
-}
+// let neighborhoodId = 0
+// let customerId = 0
+// let mealId = 0
+// let deliveryId = 0
+// 
+// class Neighborhood {
+//   constructor(name) {
+//     this.name = name
+//     this.id = neighborhoodId++
+//     store.neighborhoods.push(this)
+//   }
+// 
+//   deliveries() {
+//     return store.deliveries.filter(delivery => {
+//       return.delivery.neighborhoodId === this.id
+//     })
+//   }
+//   customers() {
+//     return store.deliveries().map(delivery => {
+//       return.delivery.customer()
+//     })
+//   }
+// }
+// 
+// class Customer {
+//   constructor(name, neighborhood) {
+//     this.name = name
+//     this.neighborhoodId = neighborhood.id
+//     this.id = customerId++
+//     store.customers.push(this)
+//   }
+// }
+// 
+// class Meal {
+//   constructor(title, price) {
+//     this.title = title
+//     this.price = price
+//     this.id = mealId++
+//     store.meals.push(this)
+//   }
+// }
+// 
+// class Delivery {
+//   constructor(mealId, neighborhoodId, customerId) {
+//     this.mealId = meal.id
+//     this.neighborhoodId = neighborhood.id
+//     this.customerId = customer.id
+//     this.id = deliveryId++
+//     store.delivery.push(this)
+//   }
+// }
