bf72c297eece10a81d95f83df3d837cccdca9ae5
Lara Sanders <larasanders@gmail.com>
2018-06-24 19:58:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 let neighborhoodId = 0
 let customerId = 0
+let mealId = 0
+let deliveryId = 0
+
 class Neighborhood {
   constructor(name) {
     this.name = name
