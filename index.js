124e38de5092bff119e79c11669299502ce6f742
Lara Sanders <larasanders@gmail.com>
2018-06-24 19:51:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
-class Neighborhood {
-  constructor(name) {
-    this.name = name
-    this.id = neighborhoodId
-    store.neighborhoods.push(this)
-  }
-}
-
-class Customer {
-  constructor(name, neighborhood) {
-    this.name = name
-    this.neighborhoodId = neighborhood.id
-    this.id = customerId
-    store.neighborhoods.push(this)
-  }
-}
+// class Neighborhood {
+//   constructor(name) {
+//     this.name = name
+//     this.id = neighborhoodId
+//     store.neighborhoods.push(this)
+//   }
+// }
+// 
+// class Customer {
+//   constructor(name, neighborhood) {
+//     this.name = name
+//     this.neighborhoodId = neighborhood.id
+//     this.id = customerId
+//     store.neighborhoods.push(this)
+//   }
+// }
