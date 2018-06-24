245f79c2f89ceb2b3eb16fc52808fc239c5e0853
Lara Sanders <larasanders@gmail.com>
2018-06-24 21:57:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ class Neighborhood {
     store.neighborhoods.push(this)
   }
 
-  // deliveries() {
-  //   return store.deliveries.filter(delivery => {
-  //     return.delivery.neighborhoodId === this.id
-  //   })
-  // }
+  deliveries() {
+    return store.deliveries.filter(delivery => {
+      return.delivery.neighborhoodId === this.id
+    })
+  }
   customers() {
       return this.neighborhood().map(neighborhood => {
         return customer.neighborhoodId === this.id
