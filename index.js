ac45da43f8b9cbdf2f4e049bef862b9d1fe4ff95
Lara Sanders <larasanders@gmail.com>
2018-06-24 20:05:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ class Neighborhood {
       return.delivery.neighborhoodId === this.id
     })
   }
+  customers() {
+    return store.deliveries().map(delivery => {
+      return.delivery.customer()
+    })
+  }
 }
 
 class Customer {
