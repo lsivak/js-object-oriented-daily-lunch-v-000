11d6398f61bd7d9326e35d7732b398e2ace7db36
Lara Sanders <larasanders@gmail.com>
2018-06-25 19:38:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
-let neighborhoodId = 0
+let neighborhoodId = 1
 let customerId = 0
 let mealId = 0
 let deliveryId = 0
