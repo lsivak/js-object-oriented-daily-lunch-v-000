9df031bcb0f987f50d90fcc5f0a8b895701a4f36
Lara Sanders <larasanders@gmail.com>
2018-06-26 18:00:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let mealId = 0
 let deliveryId = 0
 
 const Neighborhood = (() => {
-return class {
+return class Neighborhood {
   constructor(name) {
     this.name = name
     this.id = neighborhoodId++
