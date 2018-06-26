d51e0fa3a0bfc28fab1bb2552409073018937157
Lara Sanders <larasanders@gmail.com>
2018-06-26 19:25:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,7 +47,8 @@ class Customer {
     })
   }
   meals() {
-    return store.meals.find(meal => meal.id === this.mealId)
+    return store.meals.find(meal =>
+      return meal.id === this.mealId)
   }
   byPrice() {
     return this.meals().reduce((total, meal) => (total += meal.price), 0)
