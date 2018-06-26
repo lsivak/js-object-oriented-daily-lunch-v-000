1c3d5feca15f2f9902291d15f1f0abba417482a0
Lara Sanders <larasanders@gmail.com>
2018-06-26 19:02:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,9 +46,9 @@ class Customer {
 
     })
   }
-  meals() {
-    return.this.deliveries().map(delivery => delivery.meal)
-  }
+  // meals() {
+  //   return.this.deliveries().map(delivery => delivery.meal)
+  // }
 }
 
 class Meal {
