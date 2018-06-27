d4f2bd1881e77ede9c4713acf461258e3fa27cc1
Lara Sanders <larasanders@gmail.com>
2018-06-27 14:59:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 let neighborhoodId = 1
-let customerId = 0
-let mealId = 0
-let deliveryId = 0
+let customerId = 1
+let mealId = 1
+let deliveryId = 1
 
 
 class Neighborhood {
