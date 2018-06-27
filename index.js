b7c39f1a46272ff98043b5e07e076a478ac811a6
Lara Sanders <larasanders@gmail.com>
2018-06-27 19:33:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ class Customer {
   }
 
   deliveries() {
-    return store.deliveries.filter(delivery =>
-       delivery.customerId === this.id
+    return store.deliveries.filter(delivery => 
+      return delivery.customerId === this.id
 
     )
   }
