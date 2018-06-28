f2076604c008f6f08f476db4cad98e5ff3802621
Lara Sanders <larasanders@gmail.com>
2018-06-28 16:08:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,30 +55,29 @@ class Customer {
   }
 }
 
-const Meal = (() => {
-  let mealIds = 1;
-  return class {
-    constructor(title, price = 0) {
-      this.id = mealIds++;
-      this.title = title;
-      this.price = price;
-      store.meals.push(this);
+class Meal {
+  constructor(title, price) {
+    this.title = title
+    this.price = price
+    this.id = mealId++
+    store.meals.push(this)
+  }
+  deliveries() {
+    return store.deliveries.filter(delivery => {
+      return delivery.mealId === this.id
+    })
+  }
+  customers() {
+      return store.customers.filter(customer => {
+        return customer.mealId === this.id
+      })
     }
+  byPrice() {
+    return store.meals.sort((a, b) => a.price < b.price)
+      return meal.price.desc
 
-    deliveries() {
-      return store.deliveries.filter(delivery => delivery.mealId === this.id);
-    }
-
-    customers() {
-      const allCustomers = this.deliveries().map(delivery => delivery.customer());
-      return [...new Set(allCustomers)];
-    }
-
-    static byPrice() {
-      return store.meals.sort((a, b) => a.price < b.price);
-    }
-  };
-})();
+  }
+}
 class Delivery {
   constructor(mealId, neighborhoodId, customerId) {
     this.mealId = mealId
