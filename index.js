4ee1993321e6828c7005fc6184b0c539c4f2c06f
Lara Sanders <larasanders@gmail.com>
2018-06-28 15:00:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
-const Neighborhood = (() => {
-  let neighborhoodIds = 1;
-  return class {
-    constructor(name) {
-      this.id = neighborhoodIds++;
-      this.name = name;
-      store.neighborhoods.push(this);
+let neighborhoodId = 1
+let customerId = 1
+let mealId = 1
+let deliveryId = 1
+
+
+class Neighborhood {
+  constructor(name) {
+    this.name = name
+    this.id = neighborhoodId++
+    store.neighborhoods.push(this)
+  }
+
+  customers() {
+      return store.customers.filter(customer => {
+        return customer.neighborhoodId === this.id
+      })
     }
 
-    customers() {
-      return store.customers.filter(customer => customer.neighborhoodId === this.id);
+  meals() {
+      return this.customers().map(customer => {
+        return customer.meals()
+      })
     }
 
-    meals() {
-      const allMeals = this.customers().map(customer => customer.meals());
-      const merged = [].concat.apply([], allMeals);
-      return [...new Set(merged)];
-    }
-
-    deliveries() {
-      return store.deliveries.filter(delivery => delivery.neighborhoodId === this.id);
-    }
-  };
-})();
+      deliveries() {
+        return store.deliveries.filter(delivery => {
+          return delivery.neighborhoodId === this.id;
+        })
+        debugger
+      }
+  }
 
 class Customer {
   constructor(name, neighborhoodId) {
