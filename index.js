0d8541626e1a2e6af98ae02aaa282f49df7d095c
Lara Sanders <larasanders@gmail.com>
2018-06-29 20:52:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ class Neighborhood {
 
     meals() {
       const allMeals = this.customers().map(customer => customer.meals())
-      const meal = _.uniq(allMeals, meal)
+      const meal = unique(allMeals, meal)
     }
 
       deliveries() {
