a4afa12e1275f29964cb432894f2cda22d5f1c6d
Lara Sanders <larasanders@gmail.com>
2018-06-29 20:32:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ class Neighborhood {
     }
 
     meals() {
-      const allMeals = store.customers().map(customer => customer.meals())
+      const allMeals = this.customers().map(customer => customer.meals())
       return allMeals
     }
 
