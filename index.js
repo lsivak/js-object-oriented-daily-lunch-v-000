57e01e59733ba85ef1b8791bf34dcbc179a7d78c
Lara Sanders <larasanders@gmail.com>
2018-07-02 17:30:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ class Neighborhood {
     meals() {
       const allMeals = this.deliveries().map(delivery => delivery.mealId)
       // return allMeals.reduce(function(a,b){if(a.indexOf(b)<0)a.push(b);return a;},[]);
-      return _.uniq(allMeals)
+      return .uniq(allMeals)
     }
 
       deliveries() {
